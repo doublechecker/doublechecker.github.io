544d9b8e4de22ee58ebdbf00eb05af695f151020
Robin Kolze <robin.kolze@doublecheck-it.de>
2025-06-06 12:44:49 +0200

    refactoring

---
+++ assets/email-subscribe.js
@@ -0,0 +1,32 @@
+function subscribeToMaillist(endpoint, subscribeObj, callback) {
+  var isLocal = window.location.hostname === 'localhost' || window.location.hostname === '';
+  if (isLocal) {
+    console.log('Running in local environment, not sending email');
+    setTimeout(() => {
+      callback(true);
+    }, 2000);
+  } else {
+    // send the email to the server
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', endpoint, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          if (response.error) {
+            callback(false);
+          } else {
+            callback(true);
+          }
+        } else {
+          console.error('Request failed with status:', xhr.status);
+        }
+      }
+    };
+
+    var data = JSON.stringify(subscribeObj);
+    xhr.send(data);
+  }
+}
